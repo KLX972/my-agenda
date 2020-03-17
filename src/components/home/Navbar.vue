1be896d2ff6712ca15c5fa4b0551881823de1dac
Fabrice Gabriel Calixte <f.gabrielcalixte@gmail.com>
2020-03-17 17:22:49 +0100

    templating navbar

--- src/components/home/Navbar.vue
+++ src/components/home/Navbar.vue
@@ -3,13 +3,26 @@
     <div class="my-navbar-logo-content">
       <IconBase
           icon-name="arrow"
-          icon-color="black"
+          icon-color="#193E34"
           width="35"
           height="35"
       >
           <CalendarIcon/>
       </IconBase>
+      <p class="my-navbar-logo-title">Agenda</p>
     </div>
+    <div class="my-navbar-today-button-content">
+      <p class="my-navbar-today-button-text">Aujourd'hui</p>
+    </div>
+    <div class="my-navbar-change-week-buttons-content">
+      <div class="my-navbar-change-week-button-content">
+        <font-awesome-icon icon="chevron-left" class="my-navbar-change-week-button-icon"/>
+      </div>
+      <div class="my-navbar-change-week-button-content">
+        <font-awesome-icon icon="chevron-right" class="my-navbar-change-week-button-icon"/>
+      </div>
+    </div>
+    <p class="my-navbar-date">Mars 2020</p>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -3,8 +3,27 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueMoment from 'vue-moment'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faChevronDown);
+library.add(faChevronRight);
+library.add(faChevronLeft);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false
+
+// moment french language
+const moment = require('moment')
+require('moment/locale/fr')
+Vue.use(VueMoment, {
+    moment
+})
 
 /* eslint-disable no-new */
 new Vue({
